d61251fb55647ffb486bb0d273dc8adaef4b5477
Kevin Tsai <hokichaio@hotmail.com>
2014-02-24 11:28:42 +0900

    allow iframe and change post order

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(:default, Rails.env)
 
 module R4
   class Application < Rails::Application
+    config.action_dispatch.default_headers = {
+      'X-Frame-Options' => 'ALLOWALL'
+    }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
